7d8698d76afb8c1856741e76eeec0c5c023f02ca
maira32 <mairatahir3@gmail.com>
2026-07-06 13:40:30 +0500

    m

--- server.js
+++ server.js
@@ -16,4 +16,11 @@ app.get('/', (req, res) => {
 app.use('/api', authRoutes);
 app.use('/api', contactRoutes);
 
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 5000;
+  app.listen(PORT, () => {
+    console.log(`Local server is running on http://localhost:${PORT}`);
+  });
+}
+
 module.exports = app;
